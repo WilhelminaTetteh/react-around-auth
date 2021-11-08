857fd8a00ed977003b3830e9c471fb01d0a3c07a
Wilhelmina Tetteh <yemoleytetteh333@gmail.com>
2021-11-08 23:55:35 +0000

    refactor register

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import logo from "../images/logo.svg";
-
+import { Link } from "react-router-dom";
 function Header() {
   return (
     <header className="header">
       <img className="logo" src={logo} alt="logo" />
+      <div style={{ display: "flex" }}>
+        {/* <p>email@email.com</p> */}
+        <Link to="/signup" className="header__signup">
+          Signup
+        </Link>
+      </div>
     </header>
   );
 }
